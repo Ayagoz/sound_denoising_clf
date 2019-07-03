523c995cc787635294d18727670fe239981ce68a
Ayagoz <ayagoz.musabaeva@mail.ru>
2019-07-03 16:30:23 +0300

    reduce folder

--- SCD/__init__.py
+++ SCD/__init__.py
@@ -1,1 +1,5 @@
-from .scripts import *
+from .datasets import *
+from .batch_iter import *
+from .classifier import *
+from .denosing import *
+from .train import *
--- SCD/scripts/__init__.py
+++
@@ -1,5 +0,0 @@
-from .datasets import *
-from .batch_iter import *
-from .classifier import *
-from .denosing import *
-from .train import *
